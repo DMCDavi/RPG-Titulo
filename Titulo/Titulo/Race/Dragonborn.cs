3610caf95a2237261059a2daedd6b304abf3730d
guilhermevergne <51097229+guilhermevergne@users.noreply.github.com>
2020-03-13 17:28:00 -0300

    União das raças na pasta

---
+++ Titulo/Titulo/Race/Dragonborn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Titulo
+{
+    class Dragonborn : IRace
+    {
+        public void AtributeInc(Personagem Self)
+        {
+            Self.Atribute["STR"] -= 2;
+            Self.Atribute["DEX"] += 1;
+            Self.Atribute["CON"] += 1;
+            Self.Atribute["INT"] += 2;
+            Self.Atribute["WIS"] += 1;
+            Self.Atribute["CHA"] += 2;
+        }
+
+        public void Language(Personagem Self)
+        {
+            Self.LearnLang("Common");
+            Self.LearnLang("Dragonborn");
+        }
+
+        public void Speed(Personagem Self)
+        {
+            Self.TotalMove = 6;
+        }
+    }
+}
---
+++ Titulo/Titulo/Race/Dwarf.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Titulo
+{
+    class Dwarf : IRace
+    {
+        public void AtributeInc(Personagem Self)
+        {
+            Self.Atribute["STR"] -= 2;
+            Self.Atribute["DEX"] += 1;
+            Self.Atribute["CON"] += 1;
+            Self.Atribute["INT"] += 2;
+            Self.Atribute["WIS"] += 1;
+            Self.Atribute["CHA"] += 2;
+        }
+
+        public void Language(Personagem Self)
+        {
+            Self.LearnLang("Common");
+            Self.LearnLang("Dwarf");
+        }
+
+        public void Speed(Personagem Self)
+        {
+            Self.TotalMove = 6;
+        }
+    }
+}
