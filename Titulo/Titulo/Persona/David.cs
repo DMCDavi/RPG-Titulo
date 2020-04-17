efe33d55c46cf7f5345ac918da82a968c2bda1e9
jfdac11 <41966837+jfdac11@users.noreply.github.com>
2020-04-17 19:19:23 -0300

    Persona

---
+++ Titulo/Titulo/Persona/David.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Titulo
+{
+    class David : IPersona
+    {
+        public void AtributeInc(Personagem Self)
+        {
+            Self.Atribute["STR"] += 2;
+            Self.Atribute["DEX"] -= 3;
+            Self.Atribute["CON"] += 5;
+            Self.Atribute["INT"] += 3;
+            Self.Atribute["WIS"] += 1;
+            Self.Atribute["CHA"] -= 3;
+        }
+
+        public void PersonaModify(Personagem Self)
+        {
+            Self.TotalMove += 0;
+
+        }
+
+        public void SetSpritePersona(Personagem Self)
+        {
+            Self.SpritePath = "Sprite/David/";
+        }
+    }
+}
---
+++ Titulo/Titulo/Persona/Joao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Titulo
+{
+    class Joao : IPersona
+    {
+        public void AtributeInc(Personagem Self)
+        {
+            Self.Atribute["STR"] += 10;
+            Self.Atribute["DEX"] -= 1;
+            Self.Atribute["CON"] += 10;
+            Self.Atribute["INT"] += 10;
+            Self.Atribute["WIS"] += 10;
+            Self.Atribute["CHA"] -= 1;
+        }
+
+        public void PersonaModify(Personagem Self)
+        {
+            Self.TotalMove += 0;
+
+        }
+
+        public void SetSpritePersona(Personagem Self)
+        {
+            Self.SpritePath = "Sprite/Joao/";
+        }
+    }
+}
