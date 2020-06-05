daeb8ba117f43209365f15ae8958036e39b35466
Davi Costa <davi.machado.costa@hotmail.com>
2020-06-05 14:11:23 -0300

    Inserindo todas as histórias das respostas

--- Titulo/Titulo UWP/PersonalityTest.xaml.cs
+++ Titulo/Titulo UWP/PersonalityTest.xaml.cs
@@ -27,6 +27,7 @@ namespace Titulo_UWP
         int num_panel = 1, num_answer = 1;
         public List<StackPanel> AllQuestionPanels;
         Personality PTest = new Personality();
+        public StackPanel[,] AllAnswerPanels;
         public PersonalityTest()
         {
             this.InitializeComponent();
@@ -44,6 +45,18 @@ namespace Titulo_UWP
                 Panel_11,
                 Panel_12
             };
+            AllAnswerPanels = new StackPanel[10, 8] {
+                 {Answer_1_1, Answer_1_2, Answer_1_3, Answer_1_4, Answer_1_5, Answer_1_6, null, null },
+                 {Answer_2_1, Answer_2_2, Answer_2_3, Answer_2_4, Answer_2_5, null, null, null },
+                 {Answer_3_1, Answer_3_2, Answer_3_3, Answer_3_4, Answer_3_5, null, null, null },
+                 {Answer_4_1, Answer_4_2, Answer_4_3, Answer_4_4, Answer_4_5, Answer_4_6, null, null },
+                 {Answer_5_1, Answer_5_2, Answer_5_3, Answer_5_4, Answer_5_5, Answer_5_6, null, null },
+                 {Answer_6_1, Answer_6_2, null, null, null, null, null, null },
+                 {Answer_7_1, Answer_7_2, Answer_7_3, Answer_7_4, Answer_7_5, Answer_7_6, Answer_7_7, Answer_7_8 },
+                 {Answer_8_1, Answer_8_2, Answer_8_3, Answer_8_4, null, null, null, null },
+                 {Answer_9_1, Answer_9_2, null, Answer_9_4, Answer_9_5, null, null, null },
+                 {Answer_10_1, Answer_10_2, Answer_10_3, Answer_10_4, null, null, null, null },
+        };
         }
 
         private void NextStep_Click(object sender, RoutedEventArgs e)
@@ -109,12 +122,22 @@ namespace Titulo_UWP
                 {
                     if (num_panel == panel.index + 2)
                     {
-                        //Diminuiu-se 1 unidade de num_panel porque existe um painel antes de iniciarem as perguntas
-                        PTest.TestaPersona(num_panel - 1, num_answer);
-                        panel.value.Visibility = Visibility.Collapsed;
-                        AllQuestionPanels[panel.index + 1].Visibility = Visibility.Visible;
-                        num_answer = 1;
-                        num_panel++;
+                        //Caso o painel da pergunta esteja aberto, o sistema colapsa o painel, contabiliza a resposta do usuário e torna visivel a história da resposta
+                        if (panel.value.Visibility == Visibility.Visible)
+                        {
+                            //Diminuiu-se 1 unidade de num_panel porque existe um painel antes de iniciarem as perguntas
+                            PTest.TestaPersona(num_panel - 1, num_answer);
+                            panel.value.Visibility = Visibility.Collapsed;
+                            AllAnswerPanels[num_panel - 2, num_answer - 1].Visibility = Visibility.Visible;
+                        }
+                        //Caso o painel da pergunta não esteja aberto, o sistema colapsa o painel da resposta e torna visível o painel da próxima pergunta
+                        else
+                        {
+                            AllAnswerPanels[num_panel - 2, num_answer - 1].Visibility = Visibility.Collapsed;
+                            num_answer = 1;
+                            num_panel++;
+                            AllQuestionPanels[panel.index + 1].Visibility = Visibility.Visible;
+                        }
                         return;
                     }
                 }
