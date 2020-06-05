d607a575849063f2cfacb58d6696759a93f98074
jfdac11 <41966837+jfdac11@users.noreply.github.com>
2020-06-05 12:44:43 -0300

    lapa

---
+++ Titulo/TituloCore/Persona/Lapa.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TituloCore.Persona
+{
+    class Lapa : IPersona
+    {
+        public int[] CorrectAnswers = new int[10] { 4, 5, 4, 6, 3, 2, 6, 3, 4, 3 };
+        private int PersonalityPoint = 0;
+        public string Name = "Lapa";
+        public void AtributeInc(Character Self)
+        {
+            Self.Atribute["STR"] += 999;
+            Self.Atribute["DEX"] += 999;
+            Self.Atribute["CON"] += 999;
+            Self.Atribute["INT"] += 999;
+            Self.Atribute["WIS"] += 999;
+            Self.Atribute["CHA"] += 999;
+        }
+
+
+        public int PersonalityPoints()
+        {
+            return PersonalityPoint;
+        }
+
+        public int PersonalityChoice(int num_pergunta)
+        {
+            return CorrectAnswers[num_pergunta - 1];
+        }
+
+        public void IncrementPersonalityPoints()
+        {
+            PersonalityPoint++;
+        }
+
+        public void PersonaModify(Character Self)
+        {
+            Self.TotalMove += 0;
+
+        }
+
+        public void SetSpritePersona(Character Self)
+        {
+            Self.SpritePath = "Sprite/Lapa/";
+        }
+
+        public string PersonaName()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Titulo/TituloCore/Race/God.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TituloCore.Race
+{
+    public class God : IRace
+    {
+        public void AtributeInc(Character Self)
+        {
+            Self.Atribute["STR"] -= 2;
+            Self.Atribute["DEX"] += 1;
+            Self.Atribute["CON"] += 1;
+            Self.Atribute["INT"] += 2;
+            Self.Atribute["WIS"] += 1;
+            Self.Atribute["CHA"] += 2;
+        }
+
+        public void Language(Character Self)
+        {
+            Self.LearnLang("Todas");
+            Self.LearnLang("God");
+        }
+
+        public void Speed(Character Self)
+        {
+            Self.TotalMove = 6;
+        }
+    }
+}
