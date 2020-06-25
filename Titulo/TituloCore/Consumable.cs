774117f7a2e604e19f71d771629ba28ce9fbe9dd
gean-carlos718 <57230863+gean-carlos718@users.noreply.github.com>
2020-06-25 19:43:42 -0300

    Item-Back 1.0

---
+++ Titulo/TituloCore/Consumable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace TituloCore
+{
+	[DataContract(Name = "Consumable", Namespace = "http://www.contoso.com")]
+	public abstract class Consumable : Item
+	{
+		public int Value;
+		public abstract void Use(Character Target);
+	}
+}
